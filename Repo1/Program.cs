b34067e911f7cafc312d18200c14697bf1e5ed71
PIBO0122022 Фомина Елизавета Максимовна <PIBO0122022@sibupk.su>
2024-02-10 19:38:15 +0700

    test1

---
+++ Repo1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo1
+{
+    class Teacher
+    {
+        public string name;
+    }
+    class Program
+    {
+        static bool LeapYear(int year)
+        {
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0 ))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main()
+        {
+            Teacher t = new Teacher();
+            t.name = "fsdfsdf"; 
+
+            Console.Write("Введите год: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (LeapYear(year))
+            {
+                Console.WriteLine(year + " високосный год");
+            }
+            else
+            {
+                Console.WriteLine(year + " не високосный год");
+            }
+        }
+    }
+}
